9e86b6bc360ed48044637ae68823a91fef0c80ad
IrinaRiz <irinarizvanova160@gmail.com>
2021-10-20 22:17:03 -0400

    more refactoring

--- src/main/java/hw15_16/BeerSong.java
+++ src/main/java/hw15_16/BeerSong.java
@@ -4,20 +4,37 @@ public class BeerSong {
     public static void countBottles() {
 
         int count = 99;
+        String bottlesOfBeer = " bottles of beer";
+        String onTheWall = " on the wall, ";
+        String onTheWallDot = " on the wall.";
+        String passItAround = "Take one down and pass it around, ";
+        String noMore = "no more";
+        String singleBottle = " bottle of beer.";
         for (int i = count; i >= 0; i--) {
             if (i == 0) {
-                System.out.println("No more bottles of beer on the wall, no more bottles of beer.");
-                System.out.println("Go to the store and buy some more," + " " + count + " bottles of beer on the wall.");
-            } else {
-                System.out.print(i + " bottles of beer on the wall, ");
-                System.out.println(i + " bottles of beer.");
-                System.out.print("Take one down and pass it around, ");
-                System.out.println(i - 1 + " bottles of beer on the wall.");
+                System.out.println("No more "  + bottlesOfBeer + onTheWall +  noMore + " " + bottlesOfBeer + ".");
+                System.out.println("Go to the store and buy some more," + " " + count + " " + bottlesOfBeer + ".");
+                continue;
+            }
+            if ( i == 1){
+                System.out.println(i + singleBottle + onTheWall  + i  + singleBottle);
+                System.out.println(passItAround + noMore + bottlesOfBeer + onTheWallDot);
+                System.out.println();
+                continue;
+            }
+            if( i == 2){
+                System.out.println(i +  bottlesOfBeer + onTheWall + i  + bottlesOfBeer + ".");
+                System.out.print(passItAround);
+                System.out.println(i - 1 + singleBottle + onTheWallDot);
+                System.out.println();
+                continue;
+            }
+                System.out.println(i +  bottlesOfBeer + onTheWall + i  + bottlesOfBeer + ".");
+                System.out.print(passItAround);
+                System.out.println(i - 1 + bottlesOfBeer + onTheWallDot);
                 System.out.println();
             }
         }
-    }
-
 
     public static void main(String[] args) {
         countBottles();
