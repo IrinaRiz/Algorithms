7685ad5e3b65270f2954cee305ceae9cb8bc2b6f
IrinaRiz <irinarizvanova160@gmail.com>
2021-12-19 13:33:34 -0500

    refactoring

--- src/main/java/algorithms/Fibonacci.java
+++ src/main/java/algorithms/Fibonacci.java
@@ -1,8 +1,11 @@
 package algorithms;
 
+import java.util.Arrays;
+
 public class Fibonacci {
-    public static int fib (int n){
-        if (n == 0){
+    //определение числа Фибоначчи на n-позиции
+    public static int fib(int n){
+        if(n == 0){
             return 0;
         }
         if(n == 1){
@@ -13,7 +16,52 @@ public class Fibonacci {
         }
     }
 
+    public static int fibbonacci(int n){
+        if (n == 0 || n == 1){
+            return 1;
+        }
+        else{
+            return fibbonacci( n - 1) + fibbonacci(n - 2);
+        }
+    }
+
+    //нерекурсивный метод
+    public static int fibb(int n){
+        if(n == 0){
+            return 0;
+        }
+        if( n == 1){
+            return 1;
+        }
+        int a = 0;
+        int b = 1;
+        int sum =  a + b;
+        while(n > 1){
+            sum = a + b;
+            a = b;
+            b = sum;
+            n--;
+        }
+        return sum;
+    }
+
+    // построить массив с числами Фибоначчи
+    public static int[] fibonacciArray(int n){
+        int[] arr = new int[n];
+        arr[0] = 0;
+        arr[1] = 1;
+        for (int i = 2; i < arr.length; i++){
+            arr[i] = arr[i - 1] + arr[ i - 2];
+        }
+        return arr;
+
+    }
+
+
     public static void main(String[] args) {
         System.out.println(fib(7));
+        System.out.println(fibbonacci(7));
+        System.out.println(fibb(7));
+        System.out.println(Arrays.toString(fibonacciArray(9)));
     }
 }
